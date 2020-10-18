5acc4da63cf32204e22295fd314efd6646efce60
Hendika N <hendika.new@gmail.com>
2020-10-18 11:54:22 +0700

    Add rss capabilities

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,129 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-app-bar color="white" dense flat app>
+      <v-toolbar-title @click.stop="drawer = !drawer">Feed</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn @click="$store.dispatch('init')" icon>
+        <v-icon>mdi-refresh</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      color="grey lighten-3"
+      app
+      permanent
+      mini-variant
+    >
+      <v-list-item class="px-2">
+        <v-list-item-avatar>
+          <v-img :src="require('./assets/logo.svg')" contain />
+        </v-list-item-avatar>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item v-for="(item, index) in sources" :key="index" link>
+          <v-list-item-icon>
+            <v-avatar color="indigo" size="24">
+              <span
+                class="white--text"
+                v-text="item.substring(8, 10).toUpperCase()"
+              ></span>
+            </v-avatar>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <v-container>
+        <v-row dense>
+          <v-col
+            v-for="feed in allFeedItems"
+            :key="feed.title"
+            cols="12"
+            sm="6"
+            md="3"
+          >
+            <v-card ripple hover>
+              <v-card-title v-text="feed.title"></v-card-title>
+              <v-card-subtitle
+                v-text="
+                  feed.author ? feed.date + ' by ' + feed.author : feed.date
+                "
+              ></v-card-subtitle>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+
+                <v-btn icon>
+                  <v-icon>mdi-heart</v-icon>
+                </v-btn>
+
+                <v-btn icon>
+                  <v-icon>mdi-bookmark</v-icon>
+                </v-btn>
+
+                <v-btn icon>
+                  <v-icon>mdi-share-variant</v-icon>
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+export default {
+  data: () => ({
+    drawer: true,
+    options: {},
+    items: [
+      { title: "Home", icon: "mdi-home-city" },
+      { title: "My Account", icon: "mdi-account" },
+      { title: "Users", icon: "mdi-account-group-outline" },
+    ],
+  }),
+  computed: {
+    sources() {
+      return this.$store.state.sources;
+    },
+    allFeedItems() {
+      let data = [];
+      Object.keys(this.$store.state.feeds).forEach((key) => {
+        let items = this.$store.state.feeds[key];
+        data.push(...items);
+      });
+      data.sort((a, b) => a.date - b.date).reverse();
+      return data;
+    },
+    feeds() {
+      return this.$store.state.feeds;
+    },
+    cards() {
+      return this.$store.state.cards;
+    },
+  },
+  mounted() {
+    this.$store.dispatch("init");
+  },
+};
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,92 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Parser from 'rss-parser'
+
+let parser = new Parser()
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    sources: [
+      'https://codecourse.com/api/rss/courses',
+      'https://buttondown.email/alpinejs/rss'
+    ],
+    error: null,
+    feeds: {},
+    cards: [
+      {
+        title: "Pre-fab homes",
+        src: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
+        flex: 12,
+      },
+      {
+        title: "Favorite road trips",
+        src: "https://cdn.vuetifyjs.com/images/cards/road.jpg",
+        flex: 6,
+      },
+      {
+        title: "Best airlines",
+        src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
+        flex: 6,
+      },
+      {
+        title: "Pre-fab homes",
+        src: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
+        flex: 6,
+      },
+      {
+        title: "Favorite road trips",
+        src: "https://cdn.vuetifyjs.com/images/cards/road.jpg",
+        flex: 6,
+      },
+      {
+        title: "Best airlines",
+        src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
+        flex: 6,
+      },
+    ],
   },
   mutations: {
+    setData(state, data) {
+      state.cards = data
+    },
+    addError(state, error) {
+      state.error = error
+    },
+    addToFeed(state, item) {
+      state.feeds = Object.assign({}, state.feeds, { [item.url]: item.items })
+    },
   },
   actions: {
+    get({ commit }, source) {
+      let feedItem = {
+        url: source,
+        items: []
+      }
+
+      parser.parseURL(source, (err, feed) => {
+        if (err) {
+          commit('addError', err)
+        }
+
+        feed.items.forEach(entry => {
+          feedItem.items.push({
+            author: entry.author,
+            title: entry.title,
+            link: entry.link,
+            date: new Date(entry.pubDate),
+          })
+        })
+
+        commit('addToFeed', feedItem)
+      })
+    },
+    init({ dispatch }) {
+      this.state.sources.forEach(source => {
+        dispatch('get', source)
+      })
+    }
   },
   modules: {
   }
